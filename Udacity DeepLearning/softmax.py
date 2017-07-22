5c86a812e0410e5cd3c5b2c05c6bb924e50d1828
uusama <uusama.taha@yahoo.com>
2017-07-22 13:37:11 +0200

    udacity DL ..inti

---
+++ Udacity DeepLearning/softmax.py
@@ -0,0 +1,24 @@
+"""Softmax."""
+import math
+import numpy as np
+
+scores = [3.0, 1.0, 0.2]
+
+
+def softmax(x):
+    """Compute softmax values for each sets of scores in x."""
+    # scores - max For stability
+    new_scores = np.exp((scores - np.max(scores)))
+    return new_scores / np.sum(new_scores, axis=0)
+
+
+print(softmax(scores))
+
+# Plot softmax curves
+import matplotlib.pyplot as plt
+
+x = np.arange(-2.0, 6.0, 0.1)
+scores = np.vstack([x, np.ones_like(x), 0.2 * np.ones_like(x)])
+
+plt.plot(x, softmax(scores).T, linewidth=2)
+plt.show()
